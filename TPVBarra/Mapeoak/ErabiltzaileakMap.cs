f7f24a28e2473abf3c3e4f3d0f5f56edf062e971
Julen Arostegi <jul_aro_aye@goierrieskola.org>
2025-11-24 14:02:48 +0100

    Helperra sortuta eta NHibernate, fluentNHibernate eta MySql proiektuan paketeak deskargatuta

--- TPVBarra/Mapeoak/ErabiltzaileakMap.cs
+++ TPVBarra/Mapeoak/ErabiltzaileakMap.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ConsolaNHibernate.Modeloak;
+using TPVBarra.Modeloak;
 using FluentNHibernate.Mapping;
 
 namespace TPVBarra.Mapeoak
---
+++ TPVBarra/NHibernateHelper.cs
@@ -0,0 +1,63 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPVBarra
+{
+
+    using FluentNHibernate.Cfg;
+    using FluentNHibernate.Cfg.Db;
+    using NHibernate;
+    using NHibernate.Tool.hbm2ddl;
+    //using ConsolaNHibernate.Mapeoak;
+    using TPVBarra.Mapeoak;
+
+    internal class NHibernateHelper
+    {
+
+        public static class NHibernateHelperra
+        {
+            private static ISessionFactory _sessionFactory;
+
+            public static ISessionFactory SessionFactory
+            {
+                get
+                {
+                    if (_sessionFactory == null)
+                        InitializeSessionFactory();
+                    return _sessionFactory;
+                }
+            }
+
+            private static void InitializeSessionFactory()
+            {
+                _sessionFactory = Fluently.Configure()
+                    .Database(
+                        MySQLConfiguration.Standard
+                            .ConnectionString(cs => cs
+                                .Server("localhost")
+                                .Database("hibernateprobak")
+                                .Username("root")
+                                .Password("1MG2024")
+                            )
+                    )
+                    .Mappings(m =>
+                    {
+                        m.FluentMappings.AddFromAssemblyOf<ErabiltzaileakMap>();
+                    })
+                    .ExposeConfiguration(cfg => new SchemaUpdate(cfg).Execute(false, true)) // Ez dezan sortu taula exekuzio bakoitzeko
+                    .BuildSessionFactory();
+            }
+
+            public static ISession OpenSession()
+            {
+                return SessionFactory.OpenSession();
+            }
+        }
+    }
+
